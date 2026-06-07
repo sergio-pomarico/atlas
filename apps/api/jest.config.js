5eb925dd5906a5b3031083b4331c60df123a5990
Sergio Pomarico <sergiodavid21@gmail.com>
2026-06-07 09:37:09 -0500

    test: use js jest config

---
+++ apps/api/jest.config.js
@@ -0,0 +1,26 @@
+/** @type {import("jest").Config} */
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
+    "^@shared/(.*)$": "<rootDir>/src/shared/$1",
+  },
+  modulePathIgnorePatterns: [
+    "<rootDir>/src/shared/infrastructure/data/migrations",
+  ],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/setup-tests.ts"],
+  testMatch: [
+    "**/__tests__/**/*.?([mc])[jt]s?(x)",
+    "**/?(*.)+(spec|test).?([mc])[jt]s?(x)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
+
+export default config;
