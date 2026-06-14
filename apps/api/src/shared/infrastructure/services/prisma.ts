bb13f45665d65e93c5afa88215907fa5f7853042
Sergio Pomarico <sergiodavid21@gmail.com>
2026-06-14 07:48:50 -0500

    chore: refactor prisma service to improve DI

--- apps/api/src/shared/infrastructure/services/prisma.ts
+++ apps/api/src/shared/infrastructure/services/prisma.ts
@@ -4,55 +4,40 @@ import {
   type PrismaClientKnownRequestError,
   type PrismaClientUnknownRequestError,
 } from "@prisma/client/runtime/client";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { Prisma, PrismaClient } from "../../../../generated/prisma/client.ts";
+import type { SecretManagerService } from "./secret-manager.ts";
 
 export type PrismaError =
   | PrismaClientKnownRequestError
   | PrismaClientUnknownRequestError;
 
-export type DatabaseUrlProvider = () => Promise<string>;
-
 @injectable()
 export class PrismaService {
-  private static instance: PrismaService;
   private client: PrismaClient | null = null;
-  private readonly initialization: Promise<void>;
-  private readonly getDatabaseUrl: DatabaseUrlProvider;
+  private readonly secretManager: SecretManagerService;
 
-  constructor(getDatabaseUrl: DatabaseUrlProvider) {
-    this.getDatabaseUrl = getDatabaseUrl;
-    this.initialization = this.initialize().catch((err) => {
-      console.error("Error initializing PrismaService:", err);
-      throw err;
-    });
+  constructor(
+    @inject("SecretManagerService")
+    secretManager: SecretManagerService
+  ) {
+    this.secretManager = secretManager;
   }
 
-  static getInstance(getDatabaseUrl: DatabaseUrlProvider): PrismaService {
-    if (!PrismaService.instance) {
-      PrismaService.instance = new PrismaService(getDatabaseUrl);
-    }
-    return PrismaService.instance;
-  }
-
-  async initialize(): Promise<void> {
-    const databaseUrl = await this.getDatabaseUrl();
-    const adapter = new PrismaPg({ connectionString: databaseUrl });
+  async init(): Promise<void> {
+    const databaseUrl = await this.secretManager.getSecret("DATABASE_URL");
+    const adapter = new PrismaPg({ connectionString: databaseUrl.secretValue });
     this.client = new PrismaClient({ adapter });
   }
 
   getClient(): PrismaClient {
+    console.log("Getting Prisma client.", this.client);
     if (!this.client) {
       throw new PrismaClientInitializationError(
-        "PrismaService has not been initialized. Call ready() before getClient().",
+        "PrismaService has not been initialized.",
         Prisma.prismaVersion.client
       );
     }
-
     return this.client;
-  }
-
-  async ready(): Promise<void> {
-    await this.initialization;
   }
 }
